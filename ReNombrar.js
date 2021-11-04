60f4e4e3824b5b6e0ae762c8df4954fa068df755
jasbel <asbel.elmec@gmail.com>
2021-11-04 18:22:56 -0400

    rename

--- ReNombrar.js
+++ ReNombrar.js
@@ -1,13 +1,17 @@
 // todos
 db.recommends.updateMany({}, { $rename: { usuario_id: "user" } });
-db.recommends.updateMany({}, { $rename: { usuario: "profile", tipo_usuario: "userType", created_at: "createdAt" } });
-
-db.recommends.updateMany({},{ $rename: { user: "usuario_id", profile: "usuario", userType: "tipo_usuario", createdAt: "created_at" } });
+db.recommends.updateMany({}, { $rename: { usuario_id: "user", usuario: "profile", tipo_usuario: "userType", created_at: "createdAt" } });
+// db.recommends.updateMany({},{ $rename: { user: "usuario_id", profile: "usuario", userType: "tipo_usuario", createdAt: "created_at" } });
 
 // Especifico
 db.recommends.update({ _id: 1 }, { $rename: { usuario_id: "user", usuario: "profile" } });
 
 /* Renombrar una collection */
-db.recommends.renameCollection("publication_likes")
+db.anuncio_alimentos_bebidas.renameCollection("ad_fooddrinks")
+db.anuncio_animales_mascotas.renameCollection("ad_animalspets")
+db.anuncio_arte_antiguedades2.renameCollection("ad_artantiques")
+db.anuncio_hogar2.renameCollection("ad_homes")
 db.dislike_publications.renameCollection("publication_dislikes")
 db.publication_likes.renameCollection("recommends")
+
+db.anuncio_alimentos_bebidas.find()
